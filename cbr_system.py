3d96f8ea315f2e12698e463f6dab35c5474a9bfc
eaMwanika <ea.mwanika@gmail.com>
2026-07-15 19:09:21 +0300

    case bases created

---
+++ cbr_system.py
@@ -0,0 +1,83 @@
+case_base = [
+    {
+        "case_id": 1,
+        "room_type":"deluxe"
+        "complaint_category":1
+        "urgency_level":1
+        "booking_type":"online"
+        "length_of_stay":"2 nights"
+        "main_complaint":"AC not working"
+    }
+
+    {
+        "case_id": 2,
+        "room_type":"double room"
+        "complaint_category":3
+        "urgency_level":3
+        "booking_type":"physical"
+        "length_of_stay":"1 night"
+        "main_complaint":"TV not working"
+    }
+
+    {
+        "case_id": 3,
+        "room_type":"single room"
+        "complaint_category":1
+        "urgency_level":1
+        "booking_type":"online"
+        "length_of_stay":"1 night"
+        "main_complaint":"no towel"
+    }
+
+    {
+        "case_id": 4,
+        "room_type":"executive"
+        "complaint_category":1
+        "urgency_level":1
+        "booking_type":"online"
+        "length_of_stay":"4 nights"
+        "main_complaint":"AC not working"
+    }
+
+    {
+        "case_id": 5,
+        "room_type":"double room"
+        "complaint_category":"3"
+        "urgency_level":2
+        "booking_type":"online"
+        "length_of_stay":"1 night"
+        "main_complaint":"room not clean"
+    }
+
+    {
+        "case_id": 6,
+        "room_type":"double room"
+        "complaint_category":2
+        "urgency_level":3
+        "booking_type":"online"
+        "length_of_stay":"1 night"
+        "main_complaint":"no ash tray"
+    }
+
+    {
+        "case_id": 7,
+        "room_type":"single room"
+        "complaint_category":3
+        "urgency_level":1
+        "booking_type":"online"
+        "length_of_stay":"2 nights"
+        "main_complaint":"reception phone went unanswered"
+    }
+
+    {
+        "case_id": 8,
+        "room_type":"deluxe"
+        "complaint_category":1
+        "urgency_level":1
+        "booking_type":"online"
+        "length_of_stay":"5 nights"
+        "main_complaint":"fridge not working"
+    }
+
+    
+]
